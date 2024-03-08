7c6cde9bacac08e01393a5f388e95b9aad67e8b4
Sandipsutar <Sandipsutar96@yahoo.com>
2024-03-08 19:42:38 +0530

    swap assignment
    swap assignment

---
+++ 01_swapVariableAssignment.js
@@ -0,0 +1,25 @@
+console.log("=====Assignment 1.1 ======")
+console.log("=====Swap two variale Value ======")
+var sweety;
+sweety="Sweety"
+var cutie;
+cutie="Cutie"
+console.log("=====Before Swap ======")
+console.log("sweety",sweety,"cutie",cutie);
+console.log("=====After Swap ======")
+console.log("sweety",cutie,"cutie",sweety);
+
+console.log("=====Assignment 1.2 ======")
+console.log("=====Swap Three variale Value ======")
+var n1=10
+var n2=20
+var n3=30
+console.log("=====Before Swap ======")
+console.log("n1:",n1,"n2:",n2,"n3:",n3)
+console.log("=====After Swap ======")
+var temp=n1
+n1=n2;
+n2=n3;
+n3=temp;
+console.log("n1:",n2,"n2:",n1,"n3:",temp)
+
