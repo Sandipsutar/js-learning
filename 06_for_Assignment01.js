e606102e12815d0aa72bd97690ca8713f804bfbc
Sandipsutar <Sandipsutar96@yahoo.com>
2024-03-19 14:36:02 +0530

    if else function
    if else function

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,41 @@
+// 1. Print numbers from 5 to 15 by incrementing 1
+console.log("Numbers from 5 to 15:");
+for (let i = 5; i <= 15; i++) {
+    console.log(i);
+}
+
+// 2. Print numbers from 50 to 40 by decrementing 1
+console.log("Numbers from 50 to 40:");
+for (let i = 50; i >= 40; i--) {
+    console.log(i);
+}
+
+// 3. Find first 15 odd numbers
+console.log("First 15 odd numbers:");
+for (let i = 1; i <= 30; i += 2) {
+    console.log(i);
+}
+
+// 4. Find first 10 even numbers
+console.log("First 10 even numbers:");
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i);
+}
+
+// 5. Print table of 5
+console.log("Table of 5:");
+for (let i = 1; i <= 10; i++) {
+    console.log(5 * i);
+}
+
+// 6. Print table of 10
+console.log("Table of 10:");
+for (let i = 1; i <= 10; i++) {
+    console.log(10 * i);
+}
+
+// 7. Print table of 10 in reverse order
+console.log("Table of 10 in reverse order:");
+for (let i = 10; i >= 1; i--) {
+    console.log(10 * i);
+}
